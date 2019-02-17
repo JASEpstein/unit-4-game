ef7e1b869bbe2d9903b1625ebc3a3a7cc4e88b7e
JASEpstein <jasepstein@gmail.com>
2019-02-17 13:17:23 -0600

    DOM styled, assets added, JS pseudocoded

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+/* PSEUDOCODE 
+
+1. Computer generates a random number on page load
+2. Each jewel is worth a random amount
+3. User clicks a jewel icon
+4. Depending on which jewel is clicked, that random amount is added to the user's score
+5. User's score continues to increase with every jewel clicked
+6. If user's score reaches the computer's score exactly, the user wins & gets a win point
+7. If user overshoots the computer score, the user loses & gets a loss point
+8. User and computer numbers reset
+9. Values for the jewels reset
+
+*/
